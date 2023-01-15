a8718d16d6bb9d25dcb52637854253417c6ac399
rahul-mehul <rkRahul051@gmail.com>
2023-01-15 20:14:24 +0530

    code is Run

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/App.css";
 import Loader from "./Loader";
+import axios from 'axios'
 
 const LoadingStatus = {
   NOT_STARTED: "NOT_STARTED",
@@ -20,36 +21,55 @@ const App = () => {
     webiste: "",
   });
 
-  const handleOnClick = () => {};
+  const handleOnClick = () => {
+    // fetch(`${BASE_URL}/${userId}`).then(res => res.json()).then(data => console.log(data))
+    setIsLoading(LoadingStatus.IN_PROGRESS)
+    setTimeout(async () => {
+      // console.log("logging the data")
+      try {
+        let data = await axios.get(`${BASE_URL}/${userId}`)
+        let { id, email, name, phone, website } = data.data;
+        let tempUserData = { id, email, name, phone, website };
+        setUserData(tempUserData);
+        setIsLoading(LoadingStatus.SUCCESS);
+      } catch (error) {
+        setIsLoading(LoadingStatus.Error)
+        console.log(error);
+      }
+    }, 2000);
+  };
 
   const onChangeHandler = (event) => {
     setUserId(event.target.value);
   };
 
   return (
-    <div id="main">
-      <label htmlFor="number">Enter an id for the user between 1 to 100</label>
-      <input
-        type="number"
-        value={userId}
-        onChange={onChangeHandler}
-        id="input"
-        min={1}
-        max={10}
-      />
-      <button id="btn" onClick={handleOnClick}>
-        Get User
-      </button>
+    <>
+      {isLoading === LoadingStatus.IN_PROGRESS ? <Loader /> : (
+        <div id="main">
+          <label htmlFor="number">Enter an id for the user between 1 to 100</label>
+          <input
+            type="number"
+            value={userId}
+            onChange={onChangeHandler}
+            id="input"
+            min={1}
+            max={10}
+          />
+          <button id="btn" onClick={handleOnClick}>
+            Get User
+          </button>
 
-      <div id="data">
-        <h1>Click on the button to get the user</h1>
-        <h4 id="id">{userData.id}</h4>
-        <h4 id="email">{userData.email}</h4>
-        <h4 id="name">{userData.name}</h4>
-        <h4 id="phone">{userData.phone}</h4>
-        <h4 id="website">{userData.website}</h4>
-      </div>
-    </div>
+          <div id="data">
+            <h1>Click on the button to get the user</h1>
+            <h4 id="id">{userData.id}</h4>
+            <h4 id="email">{userData.email}</h4>
+            <h4 id="name">{userData.name}</h4>
+            <h4 id="phone">{userData.phone}</h4>
+            <h4 id="website">{userData.website}</h4>
+          </div>
+        </div>)}
+    </>
   );
 };
 
